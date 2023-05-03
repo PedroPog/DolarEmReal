52ae12ed0264bf2751a4f1892ea0b98a877f52a9
Pedro Vieira <pevieiraf@gmail.com>
2023-05-03 16:12:48 -0300

    Rep.
    Adicionado a função de alterar o dolar atual.

--- app/src/main/java/com/aulas/dolaremreal/MainActivity.java
+++ app/src/main/java/com/aulas/dolaremreal/MainActivity.java
@@ -2,44 +2,57 @@ package com.aulas.dolaremreal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private double dolar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize exchange rate to 0
+        dolar = 5.01;
+
+        // Set current date in textData TextView
+        TextView textData = findViewById(R.id.textData);
         SimpleDateFormat formataData = new SimpleDateFormat("dd-MM-yyyy");
-        Date data = new Date();
-        String dataFormatada = formataData.format(data);
-
-        TextView textData;
-
-        textData = findViewById(R.id.textData);
-
+        String dataFormatada = formataData.format(new Date());
         textData.setText(dataFormatada);
+
+        // Set initial exchange rate in textView2 TextView
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setText(String.format("R$ %.2f", dolar));
     }
 
-    public void converter(View view){
-        EditText editDolar;
-        TextView textView;
+    public void converter(View view) {
+        //EditText editDolar;
+        //TextView textView;
+        //editDolar = findViewById(R.id.ediDolar);
+        //textView = findViewById(R.id.textView);
 
-        editDolar = findViewById(R.id.editDolar);
-        textView = findViewById(R.id.textView);
+        EditText editDolar = findViewById(R.id.editDolar);
+        TextView textView = findViewById(R.id.textView);
 
         double valorDolar = Double.parseDouble(editDolar.getText().toString());
-        double valorConvertido = valorDolar * 5.01;
-        //textView.setText("R$ "+valorConvertido %,.2f);
-        textView.setText(String.format("R$ %.2f",valorConvertido));
+        double valorConvertido = valorDolar * dolar;
 
+        textView.setText(String.format("R$ %.2f", valorConvertido));
     }
 
+    public void mudarValor(View view) {
+        EditText editValor = findViewById(R.id.editValor);
+        TextView textView2 = findViewById(R.id.textView2);
 
+        dolar = Double.parseDouble(editValor.getText().toString());
+        textView2.setText("R$ " + dolar);
+        editValor.setText("");
+    }
 }
